e6e35acb6517d9315e09e61aef7d6611287be632
Wendell <guindaniwendell@gmail.com>
2022-12-26 17:16:22 -0300

    Primeira versão

---
+++ Exercicio de Fixacao Conta/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercicio_de_Fixacao_Conta.Entities.Exceptions;
+
+namespace Exercicio_de_Fixacao_Conta.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            if (value > WithdrawLimit)
+            {
+                throw new DomainException("The value exceeds the withdraw limit");
+            }
+            else if (Balance - value < 0)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= value;
+        }
+    }
+}
---
+++ Exercicio de Fixacao Conta/Program.cs
@@ -0,0 +1,42 @@
+﻿using Exercicio_de_Fixacao_Conta.Entities;
+using Exercicio_de_Fixacao_Conta.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_de_Fixacao_Conta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string holder;
+            double Ibalance, WithdrawL, withdraw;
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                Ibalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit:");
+                WithdrawL = double.Parse(Console.ReadLine());
+                Account account = new Account(number, holder, Ibalance, WithdrawL);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                withdraw = double.Parse(Console.ReadLine());
+                account.Withdraw(withdraw);
+                Console.WriteLine($"New balance: {account.Balance.ToString("F2")}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error. {e.Message}");
+            }
+        }
+    }
+}
